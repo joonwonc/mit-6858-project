240a02635540d84e9b70fdaf0281af03b1d3f9fd
Ubuntu <httpd@vm-6858.localdomain>
2014-12-04 22:21:55 +0000

    test.py modified

--- src/examples/withdb/test.py
+++ src/examples/withdb/test.py
@@ -3,10 +3,23 @@ sys.path.append("../../symex")
 import fuzzy
 import symsql
 
+from db import *
 from inc import *
 
 def test():
     username = fuzzy.mk_str('username')
     inc(username)
+
+    numberdb = setup("number", NumberBase)
+    user = numberdb.query(Number).get(username)
+
+    if (user == None):
+        return
+
+    usernum = user.number
+
+    if (usernum > 10):
+        print "Gotcha!"
+        return
 
 fuzzy.concolic_test(test, maxiter=2000, verbose=1)
